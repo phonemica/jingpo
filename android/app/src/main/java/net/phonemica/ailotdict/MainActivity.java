e5308e1fc2ad1535cdba26c2ada14ed55a64f8d7
柯禕藍 <yhilan.ko@gmail.com>
2017-01-25 11:06:53 +0530

    Add files via upload

---
+++ android/app/src/main/java/net/phonemica/ailotdict/MainActivity.java
@@ -0,0 +1,8 @@
+package net.phonemica.ailotdict;
+import com.facebook.react.ReactActivity;
+public class MainActivity extends ReactActivity {
+    @Override
+    protected String getMainComponentName() {
+        return "AilotDict";
+    }
+}
